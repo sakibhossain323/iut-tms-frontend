276aed27d7c31cf25073639cd3f2ac540f76c00a
sakibhossain323 <sakibhossain323@gmail.com>
2025-03-15 13:26:32 +0600

    refactor: remove callbackUrl

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -89,7 +89,6 @@ export default function LoginPage() {
                 email,
                 password,
                 redirect: false,
-                callbackUrl: "/",
             });
 
             if (response?.error) {
